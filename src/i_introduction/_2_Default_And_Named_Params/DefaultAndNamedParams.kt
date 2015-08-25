70ae2a25dea0c980f910ff88fcc713b79d4c04fc
jlbork <jlbork@mtu.edu>
2015-08-24 22:59:47 -0500

    implemented default and named params part 1

--- src/i_introduction/_2_Default_And_Named_Params/DefaultAndNamedParams.kt
+++ src/i_introduction/_2_Default_And_Named_Params/DefaultAndNamedParams.kt
@@ -10,20 +10,22 @@ fun usage() {
 }
 
 fun todoTask2_1() = TODO(
-    """
+        """
         Task 2(1).
         Rewrite all overloaded functions 'JavaCode2.foo()' to one function 'foo()' in Kotlin using default parameters.
         The function 'foo()' is declared below, you have to add parameters and replace 'todoTask2_1()' with a body.
         Uncomment the commented code and make it compile.
     """,
-    references = { name: String -> JavaCode2().foo(name); foo(name) })
+        references = { name: String -> JavaCode2().foo(name); foo(name) })
 
-fun foo(name: String): String = todoTask2_1()
+fun foo(name: String, number: Int = 42, toUpperCase: Boolean = false): String {
+    return (if (toUpperCase) name.toUpperCase () else name) + number
+}
 
 fun task2_1(): String {
-    todoTask2_1()
-//    return (foo("a") +
-//            foo("b", number = 1) +
-//            foo("c", toUpperCase = true) +
-//            foo(name = "d", number = 2, toUpperCase = true))
+    //todoTask2_1()
+    return (foo("a") +
+            foo("b", number = 1) +
+            foo("c", toUpperCase = true) +
+            foo(name = "d", number = 2, toUpperCase = true))
 }
