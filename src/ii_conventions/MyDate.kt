34f7741fe24a5ee44ee4c7ad5f55e36c3037c623
JohnBork <John.Bork@target.com>
2015-08-27 15:45:13 -0500

    finished for loop, made date iterator

--- src/ii_conventions/MyDate.kt
+++ src/ii_conventions/MyDate.kt
@@ -26,4 +26,64 @@ enum class TimeInterval {
     YEAR
 }
 
-class DateRange(public val start: MyDate, public val end: MyDate)
+class DateRange(public val start: MyDate, public val end: MyDate): Iterator<MyDate> {
+    private var previousDate = MyDate(-1, -1, -1)
+    public override fun hasNext(): Boolean {
+        if (previousDate.dayOfMonth == -1) {
+            previousDate = MyDate(start.year, start.month, start.dayOfMonth) //put date correcting code in here, next just returns date validated in hasNext
+        } else {
+            //increment day of month
+            var nextDay = previousDate.dayOfMonth + 1
+            var wasThirtyDayMonth = false
+            val thirtyDayMonths = intArrayOf(4, 6, 9, 11)
+            for (month in thirtyDayMonths.indices)
+                if (this.start.month == month) {
+                    nextDay %= 30
+                    if (nextDay == 0) nextDay = 30 //correct for next day is last day of the month
+                    wasThirtyDayMonth = true
+                    break
+                }
+
+            if (!wasThirtyDayMonth && this.start.month != 2) {
+                nextDay %= 31
+                if (nextDay == 0) nextDay = 31 //correct for next day is last day of the month
+            }
+
+
+            //is leap year
+            if (this.start.year % 4 == 0 && this.start.month == 2) {
+                nextDay %= 29
+                if (nextDay == 0) nextDay = 29 //correct for next day is last day of the month
+            }
+            else if (this.start.month == 2) {
+                nextDay %= 28
+                if (nextDay == 0) nextDay = 28 //correct for next day is last day of the month
+            }
+
+            var nextMonth = this.start.month
+            var nextYear = this.start.year
+            //check if month needs to be incremented
+            if (nextDay == 1) {
+                nextMonth = (nextMonth + 1) % 12
+                if (nextMonth == 0) nextMonth = 12 //correct for when next month is dec
+                //check for next year
+                if (nextMonth == 1) nextYear += 1
+            }
+
+            previousDate = MyDate(nextYear, nextMonth, nextDay)
+        }
+
+        return if (this.previousDate <= this.end) true else false
+    }
+
+    public override fun next(): MyDate {
+        //month: 9 4 6 11: 30 else 31 except feb 28/29 leap year 2016
+        //check if first date
+        if (previousDate == start) {
+            previousDate = MyDate(previousDate.year, previousDate.month, previousDate.dayOfMonth)
+            return start
+        }
+
+        return previousDate
+    }
+}
