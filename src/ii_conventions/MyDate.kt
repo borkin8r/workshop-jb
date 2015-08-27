6a261487bef72a70524117df436071279b40c8b3
JohnBork <John.Bork@target.com>
2015-08-27 10:52:12 -0500

    finished comparison conventions

--- src/ii_conventions/MyDate.kt
+++ src/ii_conventions/MyDate.kt
@@ -1,6 +1,24 @@
 package ii_conventions
 
-data class MyDate(val year: Int, val month: Int, val dayOfMonth: Int)
+data class MyDate(val year: Int, val month: Int, val dayOfMonth: Int) : Comparable<MyDate> {
+    public override fun compareTo(m: MyDate): Int {
+
+        if (this.year < m.year)
+            return -1
+        else if (this.year > m.year)
+            return 1
+        else if (this.month < m.month)
+            return -1
+        else if (this.month > m.month)
+            return 1
+        else if (this.dayOfMonth < m.dayOfMonth)
+            return -1
+        else if (this.dayOfMonth > m.dayOfMonth)
+            return 1
+        else
+            return 0
+    }
+}
 
 enum class TimeInterval {
     DAY
